5df747028445a9f68700740d0302d07187e937e8
huanghongxia <huanghongxia@tsj-001385.ytx.com>
2017-02-10 18:49:57 +0800

    Add all file

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Root from './root';  // import Root that we just created
+
+// rendering the Root component into <div id="App"></div>
+render(
+  <Root />,
+  document.getElementById('App')
+);
